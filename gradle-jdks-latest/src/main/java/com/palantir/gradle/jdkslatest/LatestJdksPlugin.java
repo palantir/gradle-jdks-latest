7579016d10ec8b7d45e54b30b2831592db3bd506
Kelvin Ou <kelvin.ou.jin.bin@gmail.com>
2025-06-30 17:36:24 +0100

    Only apply legacy

--- gradle-jdks-latest/src/main/java/com/palantir/gradle/jdkslatest/LatestJdksPlugin.java
+++ gradle-jdks-latest/src/main/java/com/palantir/gradle/jdkslatest/LatestJdksPlugin.java
@@ -21,8 +21,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.palantir.gradle.jdks.JdksExtension;
 import com.palantir.gradle.jdks.JdksPlugin;
 import com.palantir.gradle.jdks.PalantirCaPlugin;
+import com.palantir.gradle.jdks.enablement.GradleJdksEnablement;
 import com.palantir.gradle.jdks.json.JdksInfoJson;
 import java.io.IOException;
+import java.nio.file.Path;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -30,6 +32,11 @@ public final class LatestJdksPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getPlugins().apply(JdksPlugin.class);
+
+        if (!GradleJdksEnablement.isGradleJdkSetupEnabled(Path.of(project.getPath()))) {
+            project.getPlugins().apply(PalantirCaPlugin.class);
+        }
+
         project.getPlugins().apply(PalantirCaPlugin.class);
 
         JdksExtension jdksExtension = project.getExtensions().getByType(JdksExtension.class);
